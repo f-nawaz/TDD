189c1a1f6fd6f0bae07d556ac94b08d48498e873
Farkh <farkhnawaz@gmail.com>
2016-06-22 16:14:39 +0000

    created four tests for person class and passed them

---
+++ person_framework/person.rb
@@ -0,0 +1,23 @@
+class Person
+  attr_reader :dob, :first_name, :sur_name, :emails, :phone_numbers
+
+  def initialize(fname, sname, dob = nil)
+    @first_name =fname.capitalize
+    @sur_name = sname.capitalize
+    @dob = Date.parse(dob) if dob
+    @emails = []
+    @phone_numbers  = []
+  end
+
+  def fullname
+    "#{first_name} #{sur_name}"
+  end
+
+  def add_email(email)
+    @emails << email
+  end
+
+  def add_phone(phone)
+    @phone_numbers << phone
+  end
+end
---
+++ person_framework/spec/person_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Person class" do
+  subject {Person.new("Joe", "Bloggs","1 Jan 1990")}
+  it 'Should store and return personal information' do
+    expect(subject.dob).to eq Date.new(1990,1,1)
+    expect(subject.first_name).to eq "Joe"
+    expect(subject.sur_name).to eq "Bloggs"
+    expect(subject.fullname).to eq "Joe Bloggs"
+end
+
+  it "Should have an optional dob" do
+      person = Person.new("Joe", "Bloggs")
+      expect(person.dob).to be_nil
+  end
+
+  it "should store and return emails in an array" do
+      subject.add_email("joe@work.com")
+      subject.add_email("joe.bloggs@work.com")
+
+      expect(subject.emails).to eq ["joe@work.com", "joe.bloggs@work.com"]
+  end
+
+  it "Should store and return phone numbers in an array" do
+    subject.add_phone("07712345678")
+    subject.add_phone("02012345678")
+
+    expect(subject.phone_numbers).to eq ["07712345678","02012345678"]
+  end
+
+  it "Should remove email addresses from the array emails using the index number and then return the current phone array list" do
+    subject.add_phone("07712345678")
+    subject.add_phone("02012345678")
+    subject.remove_email(0)
+  end
+
+
+
+
+end
